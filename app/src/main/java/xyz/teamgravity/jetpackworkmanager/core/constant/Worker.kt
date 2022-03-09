bd695dce5dde7e2a5e66ab918b2afac617f4908a
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-09 16:56:35 +0500

    color filter worker

--- app/src/main/java/xyz/teamgravity/jetpackworkmanager/core/constant/Worker.kt
+++ app/src/main/java/xyz/teamgravity/jetpackworkmanager/core/constant/Worker.kt
@@ -3,4 +3,5 @@ package xyz.teamgravity.jetpackworkmanager.core.constant
 object Worker {
     const val ERROR = "error"
     const val IMAGE_URI = "image_uri"
+    const val FILTER_URI = "filter_uri"
 }
--- app/src/main/java/xyz/teamgravity/jetpackworkmanager/core/worker/ColorFilterWorker.kt
+++ app/src/main/java/xyz/teamgravity/jetpackworkmanager/core/worker/ColorFilterWorker.kt
@@ -1,4 +1,46 @@
 package xyz.teamgravity.jetpackworkmanager.core.worker
 
-class ColorFilterWorker {
+import android.content.Context
+import android.graphics.*
+import androidx.core.net.toFile
+import androidx.core.net.toUri
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import xyz.teamgravity.jetpackworkmanager.core.constant.Worker
+import java.io.File
+import java.io.FileOutputStream
+
+@Suppress("BlockingMethodInNonBlockingContext")
+class ColorFilterWorker(
+    private val context: Context,
+    private val parameters: WorkerParameters
+) : CoroutineWorker(context, parameters) {
+
+    override suspend fun doWork(): Result {
+        val file = parameters.inputData.getString(Worker.IMAGE_URI)?.toUri()?.toFile()
+        delay(5_000L)
+
+        return withContext(Dispatchers.IO) {
+            return@withContext file?.let { file ->
+                val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+                val resultBitmap = bitmap.copy(bitmap.config, true)
+
+                val paint = Paint()
+                paint.colorFilter = LightingColorFilter(0x08FF04, 1)
+                val canvas = Canvas(resultBitmap)
+                canvas.drawBitmap(resultBitmap, 0F, 0F, paint)
+
+                val resultImageFile = File(context.filesDir, "new-image.jpg")
+                val output = FileOutputStream(resultImageFile)
+                val successful = resultBitmap.compress(Bitmap.CompressFormat.JPEG, 90, output)
+
+                if (successful) Result.success(workDataOf(Worker.FILTER_URI to resultImageFile.toUri().toString()))
+                else Result.failure()
+            } ?: Result.failure()
+        }
+    }
 }
