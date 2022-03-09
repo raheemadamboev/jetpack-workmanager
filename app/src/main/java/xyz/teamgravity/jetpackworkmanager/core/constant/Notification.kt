4f91ae080653d6857ad2246d5ce654a9d6985c4f
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-09 16:42:42 +0500

    created notification channel

--- app/src/main/java/xyz/teamgravity/jetpackworkmanager/core/constant/Notification.kt
+++ app/src/main/java/xyz/teamgravity/jetpackworkmanager/core/constant/Notification.kt
@@ -3,5 +3,6 @@ package xyz.teamgravity.jetpackworkmanager.core.constant
 object Notification {
 
     const val NOTIFICATION_CHANNEL_ID = "download_channel"
+    const val NOTIFICATION_CHANNEL_NAME = "Download files"
     const val NOTIFICATION_ID = 77
 }
--- app/src/main/java/xyz/teamgravity/jetpackworkmanager/injection/App.kt
+++ app/src/main/java/xyz/teamgravity/jetpackworkmanager/injection/App.kt
@@ -1,9 +1,13 @@
 package xyz.teamgravity.jetpackworkmanager.injection
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import xyz.teamgravity.jetpackworkmanager.core.constant.Notification
 import javax.inject.Inject
 
 @HiltAndroidApp
@@ -11,6 +15,24 @@ class App : Application(), Configuration.Provider {
 
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                Notification.NOTIFICATION_CHANNEL_ID,
+                Notification.NOTIFICATION_CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(channel)
+        }
+    }
 
     override fun getWorkManagerConfiguration(): Configuration {
         return Configuration.Builder()
